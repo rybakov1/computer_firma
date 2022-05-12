2f19bfb34bd56ecb1e72702c1d91e530d08ceac5
main <rybvlad4@gmail.com>
2022-05-12 21:57:51 +0300

    Catalog improvements

---
+++ app_computer/Catalog.xaml.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using Microsoft.EntityFrameworkCore;
+using app_computer.Models;
+using app_computer.papochka;
+using System.Collections.ObjectModel;
+
+namespace app_computer
+{
+    public partial class Catalog : Window
+    {
+        mydbContext db;
+        ObservableCollection<ComponentMem> ComponentList { get; set; }
+
+        List<int> Id = new List<int>();
+
+        public Catalog()
+        {
+            InitializeComponent();
+            db = new mydbContext();
+            CategoriesButtonsCreate();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            string category = textBox.Text;
+            GetComponentByCategory(category);
+        }
+
+        private void GetComponentByCategory(string category)
+        {
+            var components = from u in db.Components
+                             join c in db.Typeofcomponents on u.IdType equals c.IdType
+                             where c.Title == category
+                             select new { u.IdComp, u.Model, u.Description, u.Price, u.Specifications };
+
+            ComponentList = new ObservableCollection<ComponentMem>();
+            foreach (var component in components)
+            {
+                ComponentList.Add(new ComponentMem { Id = component.IdComp, Model = component.Model, Price = (decimal)component.Price, Description = component.Description, Specifications = component.Specifications });
+            }
+            componentList.ItemsSource = ComponentList;
+        }
+
+        private void GetPresetComponents(string category)
+        {
+            var presets = db.Presets.Include(c => c.Comps).Where(c => c.Name == category)
+                        .ToList();
+
+            TextBlock[] tBlock = new TextBlock[4];
+            int i = 0;
+            box.Text = "";
+
+            foreach (var preset in presets)
+            {
+                foreach (var components in preset.Comps)
+                {
+                    tBlock[i] = new TextBlock
+                    {
+                        Text = $"{i + 1}. {components.Model} ({components.Price} руб.) - {components.Description} \n",
+                        Name = String.Format("Textblock{0}", i)
+                    };
+                    box.Inlines.Add(tBlock[i]);
+                    Grid.SetRow(tBlock[i], i);
+                    i++;
+                }
+            }
+        }
+
+        private void CategoriesButtonsCreate()
+        {
+            var components = from u in db.Typeofcomponents
+                             select new { u.Title };
+
+            var presets = db.Presets.Include(c => c.Comps)
+                        .ToList();
+
+            List<string> list_comps = new List<string>();
+            List<string> list_presets = new List<string>();
+
+            foreach (var preset in presets)
+            {
+#pragma warning disable CS8604 // Возможно, аргумент-ссылка, допускающий значение NULL.
+                list_presets.Add(preset.Name);
+#pragma warning restore CS8604 // Возможно, аргумент-ссылка, допускающий значение NULL.
+            }
+
+            foreach (var category in components)
+            {
+                list_comps.Add(category.Title);
+            }
+
+            ButtonGeneration(list_presets, stack_pc, handler: button_click2);
+            ButtonGeneration(list_comps, stack_comp, handler: button_click1);
+        }
+
+        private void ButtonGeneration(List<string> categories, StackPanel stackPanel, RoutedEventHandler handler)
+        {
+            int i = 0;
+            Button[] b = new Button[categories.Count];
+
+            foreach (var category in categories)
+            {
+                b[i] = new Button
+                {
+                    Name = $"button{i}",
+                    Content = $"{category}",
+                    FontSize = 14
+                };
+                Thickness thickness = b[i].Margin;
+                thickness.Top = 5;
+                b[i].Margin = thickness;
+
+                b[i].Click += handler;
+
+                stackPanel.Children.Add(b[i]);
+                i++;
+            }
+        }
+
+        void AddItemInCart(List<int> Ids)
+        {
+            box.Text += Ids;
+        }
+        private void button_click1(object sender, RoutedEventArgs e)
+        {
+            var senderBtn = sender as Button;
+#pragma warning disable CS8604 // Возможно, аргумент-ссылка, допускающий значение NULL.
+            GetComponentByCategory(senderBtn.Content.ToString());
+        }
+
+        private void button_click2(object sender, RoutedEventArgs e)
+        {
+            var senderBtn = sender as Button;
+            GetPresetComponents(senderBtn.Content.ToString());
+        }
+        private void dataTemplateButton_Click(object sender, RoutedEventArgs e)
+        {
+            var senderBtn = sender as Button;
+            Id.Add((int)senderBtn.Tag);
+            AddItemInCart(Id);
+#pragma warning restore CS8604 // Возможно, аргумент-ссылка, допускающий значение NULL.
+        }
+
+        private void cart_button_Click(object sender, RoutedEventArgs e)
+        {
+            OrderDetails orderDetails = new OrderDetails(Id);
+            App.Current.MainWindow = orderDetails;
+
+            this.Close();
+            orderDetails.Show();
+        }
+    }
+}
